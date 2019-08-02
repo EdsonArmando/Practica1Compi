ba02fda6f3aab69c9e493049e1ea55b72247f6fa
EdsonArmando <guixarmando@gmail.com>
2019-08-01 19:18:59 -0700

    Interfaz grafica completa

---
+++ src/practica1compi/Practica1Compi.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1compi;
+
+import views.Inicio;
+
+/**
+ *
+ * @author EG
+ */
+public class Practica1Compi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Inicio inicio = new Inicio();
+        inicio.setVisible(true);
+    }
+    
+}
